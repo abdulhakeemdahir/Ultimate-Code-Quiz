83742e941d66cf65fec19853fe77b60d443e6635
Abdulhakeem <somlutions@gmail.com>
2020-10-30 15:36:09 -0700

    continued work on JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,45 +1,53 @@
 // Need to set up all of my variables
 let questions = [{
     question: "Question number 1",
-    choice1: "Choice 1",
-    choice2: "Choice 2",
-    choice3: "Choice 3",
-    choice4: "Choice 4",
-    answer: this.choice1,
+    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
+    answer: 1
 },
 {
-    question: "Question number 2",
-    choice1: "Choice 5",
-    choice2: "Choice 6",
-    choice3: "Choice 7",
-    choice4: "Choice 8",
-    answer: this.choice1,
-},{
-    question: "Question number 3",
-    choice1: "Choice 9",
-    choice2: "Choice 10",
-    choice3: "Choice 11",
-    choice4: "Choice 12",
-    answer: this.choice1,
+    question: "Question number 1",
+    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
+    answer: 1
 },
 {
-    question: "Question number 4",
-    choice1: "Choice 13",
-    choice2: "Choice 14",
-    choice3: "Choice 15",
-    choice4: "Choice 16",
-    answer: this.choice1,
-},]
-let container = document.querySelector("#container");
-let count = document.querySelector("#count").textContent;
+    question: "Question number 1",
+    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
+    answer: 1
+},
+{
+    question: "Question number 1",
+    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
+    answer: 1
+},
+]
+let containerEl = document.querySelector("#container");
+let startButtonEl = document.querySelector("#btn-start");
+let countEl = document.querySelector("#count");
+let count = 75;
 let i = 0;
 
 // Need to create all of my functions and logic
 function startTime () {
+    startButtonEl.setAttribute("style", "display:none;");
     let timerInterval = setInterval(function(){
-
+        startQuestions();
+        count--;
+        countEl.textContent = count;
+        if (count === 0){
+            clearInterval(timerInterval);
+        }
+        
 
     }, 1000);
 }
 
+function startQuestions (){
+    questionEl = document.createElement("h1");
+    questionEl.textContent = questions.question[i];
+    containerEl.appendChild(questionEl);
+
+}
+
 // Need to execute my functions
+
+startButtonEl.addEventListener("click", startTime);
