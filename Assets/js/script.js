95e3962acaf3c0613f23efd64c61197e892d61db
Abdulhakeem <somlutions@gmail.com>
2020-10-30 14:41:19 -0700

    Started on JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,45 @@
 // Need to set up all of my variables
-
+let questions = [{
+    question: "Question number 1",
+    choice1: "Choice 1",
+    choice2: "Choice 2",
+    choice3: "Choice 3",
+    choice4: "Choice 4",
+    answer: this.choice1,
+},
+{
+    question: "Question number 2",
+    choice1: "Choice 5",
+    choice2: "Choice 6",
+    choice3: "Choice 7",
+    choice4: "Choice 8",
+    answer: this.choice1,
+},{
+    question: "Question number 3",
+    choice1: "Choice 9",
+    choice2: "Choice 10",
+    choice3: "Choice 11",
+    choice4: "Choice 12",
+    answer: this.choice1,
+},
+{
+    question: "Question number 4",
+    choice1: "Choice 13",
+    choice2: "Choice 14",
+    choice3: "Choice 15",
+    choice4: "Choice 16",
+    answer: this.choice1,
+},]
+let container = document.querySelector("#container");
+let count = document.querySelector("#count").textContent;
+let i = 0;
 
 // Need to create all of my functions and logic
+function startTime () {
+    let timerInterval = setInterval(function(){
 
+
+    }, 1000);
+}
 
 // Need to execute my functions
