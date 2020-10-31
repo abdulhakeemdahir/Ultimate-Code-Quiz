0df569b360815c0cddb2472edef628827c7ed9ab
Abdulhakeem <somlutions@gmail.com>
2020-10-30 17:33:39 -0700

    working on the for loop iterator for questions array

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,25 +1,26 @@
 // Need to set up all of my variables
-let questions = [{
+let questions = [
+  {
     question: "Question number 1",
-    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
-    answer: 1
-},
-{
+    choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
+    answer: 1,
+  },
+  {
     question: "Question number 2",
-    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
-    answer: 1
-},
-{
+    choices: ["Choice 5", "Choice 6", "Choice 7", "Choice 8"],
+    answer: 1,
+  },
+  {
     question: "Question number 3",
-    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
-    answer: 1
-},
-{
+    choices: ["Choice 9", "Choice 10", "Choice 11", "Choice 12"],
+    answer: 1,
+  },
+  {
     question: "Question number 4",
-    choices: ["Choice 1","Choice 2","Choice 3","Choice 4"],
-    answer: 1
-},
-]
+    choices: ["Choice 13", "Choice 14", "Choice 15", "Choice 16"],
+    answer: 1,
+  },
+];
 let containerEl = document.querySelector("#container");
 let startButtonEl = document.querySelector("#btn-start");
 let countEl = document.querySelector("#count");
@@ -27,52 +28,52 @@ let count = 75;
 let i = 0;
 
 // Need to create all of my functions and logic
-function startTime () {
-    startButtonEl.setAttribute("style", "display:none;");
-    startQuestions();
-    let timerInterval = setInterval(function(){
-        count--;
-        countEl.textContent = count;
-        if (count === 0){
-            clearInterval(timerInterval);
-        }
-        
-
-    }, 1000);
+function startTime() {
+  startButtonEl.setAttribute("style", "display:none;");
+  startQuestions();
+  let timerInterval = setInterval(function () {
+    count--;
+    countEl.textContent = count;
+    if (count === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
 }
 
+function startQuestions() {
+  questionEl = document.createElement("h1");
+  questionEl.textContent = questions[i].question;
+  containerEl.appendChild(questionEl);
+  containerEl.setAttribute(
+    "style",
+    "display:flex;flex-direction:column;text-align:center;width:30%;margin:0px auto;justify-content:space-between;"
+  );
 
+  choiceEl1 = document.createElement("button");
+  choiceEl1.textContent = questions[i].choices[0];
+  containerEl.appendChild(choiceEl1);
 
-function startQuestions (){
-    
-  
-    questionEl = document.createElement("h1");
-    questionEl.textContent = questions[0].question;
-    containerEl.appendChild(questionEl);
-    containerEl.setAttribute("style","display:flex;flex-direction:column;text-align:center;width:30%;margin:0px auto;justify-content:space-between;");
+  choiceEl2 = document.createElement("button");
+  choiceEl2.textContent = questions[i].choices[1];
+  containerEl.appendChild(choiceEl2);
 
-    choiceEl1 = document.createElement("button");
-    choiceEl1.textContent = questions[1].choices[0];
-    containerEl.appendChild(choiceEl1);
+  choiceEl3 = document.createElement("button");
+  choiceEl3.textContent = questions[i].choices[2];
+  containerEl.appendChild(choiceEl3);
 
-    choiceEl2 = document.createElement("button");
-    choiceEl2.textContent = questions[1].choices[1];
-    containerEl.appendChild(choiceEl2);
-
-    choiceEl3 = document.createElement("button");
-    choiceEl3.textContent = questions[1].choices[2];
-    containerEl.appendChild(choiceEl3);
-
-    choiceEl4 = document.createElement("button");
-    choiceEl4.textContent = questions[1].choices[3];
-    containerEl.appendChild(choiceEl4);
-    
-    containerEl.addEventListener("click", nextQuestion)
+  choiceEl4 = document.createElement("button");
+  choiceEl4.textContent = questions[i].choices[3];
+  containerEl.appendChild(choiceEl4);
 }
 
-function nextQuestion
-
+containerEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (event.target.matches("button")) {
+    for (let i = 0; i < questions.length; i++) {
+      i++;
+    }
+  }
+});
 
 // Need to execute my functions
-// startQuestions();
-startButtonEl.addEventListener("click", startTime)
+startButtonEl.addEventListener("click", startTime);
